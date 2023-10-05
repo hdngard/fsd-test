9da8307f3b7020916338cc2de20b184432169793
AndreiVaskin <Andrei.Vaskin@noveogroup.com>
2023-10-06 00:54:40 +0500

    add webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import {BuildOptions} from "./types/config";
+import {WebpackConfiguration} from "webpack-cli";
+import {buildPlugin} from "./buldPlugin";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): WebpackConfiguration {
+    const {mode, paths, isDev} = options
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugin(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+}
